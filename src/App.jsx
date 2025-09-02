eabb309f975de124c711f0ae7942424f84fe96c2
fanxiaotuGod <fhc991115@gmail.com>
2025-09-01 18:28:00 -0700

    Add scroll to top functionality when navigating between pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
 import Team from './pages/Team';
 import Events from './pages/Events';
@@ -12,6 +13,7 @@ import './App.css';
 function App() {
   return (
     <Router basename="/UBC-PIN-Web">
+      <ScrollToTop />
       <div className="App">
         <Header />
         <main className="main-content">
